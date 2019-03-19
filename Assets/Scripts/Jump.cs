e8150005c752fb655ee9c83d078b63872a1ad0ad
Dario <darioandrade@email.arizona.edu>
2019-03-19 11:45:40 -0700

    Finalized Controller for now

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -6,10 +6,11 @@ public class Jump : MonoBehaviour
 {
     public float jumpVelocity;
     public float fallMultiplier;
+    private bool inAir;
     // Start is called before the first frame update
     void Start()
     {
-        
+        inAir = false;
     }
 
     // Update is called once per frame
@@ -17,15 +18,30 @@ public class Jump : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            
-            this.GetComponent<Rigidbody>().velocity = new Vector3(0f, jumpVelocity, 0f);
+            if (!inAir)
+            {
+                this.GetComponent<Rigidbody>().velocity = new Vector3(0f, jumpVelocity, 0f);
+                inAir = true;
+            }
             
         }
-        if (this.GetComponent<Rigidbody>().velocity.y < 0) {
-            this.GetComponent<Rigidbody>().velocity = new Vector3(0f, Physics.gravity.y * fallMultiplier * Time.deltaTime, 0f);
-            
+        if (inAir)
+        {
+            if (this.GetComponent<Rigidbody>().velocity.y < 0)
+            {
+                this.GetComponent<Rigidbody>().velocity = new Vector3(0f, Physics.gravity.y * fallMultiplier * Time.deltaTime, 0f);
+
+            }
         }
        
         print(this.GetComponent<Rigidbody>().velocity.y);
+    }
+
+    public void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "floor") {
+            this.GetComponent<Rigidbody>().velocity = new Vector3(this.GetComponent<Rigidbody>().velocity.x, 0f, this.GetComponent<Rigidbody>().velocity.z);
+            inAir = false;
+        }
+
     }
 }
