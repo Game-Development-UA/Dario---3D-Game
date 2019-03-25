63f9b1ab1d6622f55d7b3aa51d89d4aeeb14c66e
Dario <darioandrade@email.arizona.edu>
2019-03-24 19:16:32 -0700

    Added Power up script and bonus platforms

---
+++ Assets/Scripts/PowerUp_Jump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp_Jump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCollisionEnter(Collision Coll) {
+
+
+        Coll.gameObject.GetComponent<Jump>().jumpVelocity = 1.5f * Coll.gameObject.GetComponent<Jump>().jumpVelocity;
+        Destroy(this.gameObject);
+
+    }
+}
