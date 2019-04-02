5b6ef6b6417a5c1b946c4205bb68b7db6dc3d453
Dario <darioandrade@email.arizona.edu>
2019-04-02 10:50:48 -0700

    Script updates and starting to map out my left segment of the level

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -8,27 +8,34 @@ public class MovePlayer : MonoBehaviour
     public Vector3 Direction;
     public GameObject Player;
     public float platSpeed;
+    public bool playerOn;
     void Start()
     {
-        
+        playerOn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        TranslatePlayer();
+        if (playerOn)
+        {
+            TranslatePlayer();
+        }
     }
 
-    public void OnTriggerEnter(Collider col) {
+    public void OnCollisionEnter(Collider col) {
         col.gameObject.GetComponent<Controller_Button>().stuckToPlat = true;
         col.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
-        Player = col.gameObject;
+        playerOn = true;
+        print("playerOn: " + playerOn);
+        //Player = col.gameObject;
     }
     public void TranslatePlayer() {
         Player.transform.Translate(new Vector3(Direction.x * Time.deltaTime * platSpeed, Direction.y * Time.deltaTime * platSpeed, Direction.z * Time.deltaTime * platSpeed));
     }
 
-    public void OnTriggerExit(Collider col) {
+    public void OnCollisionExit(Collider col) {
         col.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        playerOn = false;
     }
 }
